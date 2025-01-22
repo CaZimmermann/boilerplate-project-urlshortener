ff2f86b1d603e45202d0116bbeb9a779a2c0e296
CaZimmermann <103119813+CaZimmermann@users.noreply.github.com>
2025-01-22 15:07:38 +0100

    feat: finished project

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,8 +17,41 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+
+const shortUrl = {};
+let urlId = 1;
+
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+
+  const urlRegex = /^(http|https):\/\/[^\s$.?#].[^\s]*$/;
+  if (!urlRegex.test(originalUrl)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  const shortUrlKey = urlId++;
+  shortUrl[shortUrlKey] = originalUrl;
+
+  res.json({
+    original_url: originalUrl,
+    short_url: shortUrlKey,
+  });
+});
+
+app.get('/api/shorturl/:short_url', function(req, res) {
+  const shortUrlKey = req.params.short_url;
+  const originalUrl = shortUrl[shortUrlKey];
+
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.json({error: "No short URL found for the given input"});
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal server error' });
 });
 
 app.listen(port, function() {
